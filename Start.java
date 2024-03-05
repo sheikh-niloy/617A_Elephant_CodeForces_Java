88639e00e5826c98b86a5392715830964cb174f8
sheikh-niloy <sheikh.niloy.info@gmail.com>
2024-03-06 00:13:14 +0600

    commit

---
+++ Start.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Start {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        System.out.println((a + 4) / 5);
+    }
+}
